1bd50f2de2a8c23afc62c886490b54b753a88394
ryo_ishibashi <ryokky59@gmail.com>
2018-09-12 16:13:53 +0900

    サインアップ時、ユーザー名の入力フォーム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/schedules/top', to: 'schedules#top'
-  root to: 'schedules#top'
+  root 'schedules#top'
   devise_for :users
+  resources :users, :only => [:index, :show]
   resources :schedules
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
