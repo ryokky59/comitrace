883f9c961a6f4ef291ddd96438eff730dd600323
ryo_ishibashi <ryokky59@gmail.com>
2018-09-12 16:50:21 +0900

    ユーザーページにプロフィール文を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   get '/schedules/top', to: 'schedules#top'
   root 'schedules#top'
   devise_for :users
-  resources :users, :only => [:index, :show]
+  resources :users, :only => [:show]
   resources :schedules
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
