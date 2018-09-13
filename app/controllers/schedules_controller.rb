33e2d903f72f745b339ab017a41f6aa4a3916f41
ryo_ishibashi <ryokky59@gmail.com>
2018-09-13 15:46:38 +0900

    schedule,schedule_planテーブルに保存し、値を取得

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,13 +3,22 @@ class SchedulesController < ApplicationController
   end
 
   def index
+    @schedules = Schedule.all.includes(:schedule_plans)
   end
 
   def new
+    @schedule = Schedule.new
+    @schedule.schedule_plans.build
   end
 
   def create
-    
+    @schedule = Schedule.new(schedule_params)
+    @schedule.user_id = current_user.id
+    if @schedule.save
+      redirect_to schedules_path
+    else
+      render 'new'
+    end
   end
 
   def confirm
@@ -29,7 +38,7 @@ class SchedulesController < ApplicationController
 
   private
   def schedule_params
-    params.require(:schedule).permit(:title, :content, schedule_prans_attributes: [:plan, :start_time, :end_time])
+    params.require(:schedule).permit(:title, :content, schedule_plans_attributes: [:id,:plan, :start_time, :end_time, :_destroy])
   end
 
 
