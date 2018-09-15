1a3611b46fad55d4aeaaf4dc6c859022ef4a2642
ryo_ishibashi <ryokky59@gmail.com>
2018-09-15 16:03:11 +0900

    投稿から詳細まで作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require Chart.min
 //= require rails-ujs
-//= require turbolinks
 //= require jquery/dist/jquery.js
+//= require Chart.min
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,7 +5,9 @@ class SchedulesController < ApplicationController
   end
 
   def index
-    @schedules = Schedule.all.includes(:schedule_plans)
+    @schedules = Schedule.all
+    #@schedules = Schedule.all.includes(:schedule_plans)子要素も載せたい時
+
   end
 
   def new
@@ -13,36 +15,40 @@ class SchedulesController < ApplicationController
     @schedule.schedule_plans.build
   end
 
-  def create
-    @schedule = Schedule.new(schedule_params)
-    @schedule.user_id = current_user.id
-    if @schedule.save
-      redirect_to confirm_schedules_path
-    else
-      render 'new'
-    end
-  end
-
   def confirm
     @schedule = Schedule.new(schedule_params)
     @schedule.user_id = current_user.id
 
-    color = ["##FF0000", "#FE9A2E", "#F7FE2E", "#0040FF", "#00FF40", "#DF01D7", "#00FFFF", "#8000FF", "#BFFF00", "#045FB4"]
     gon.data = []
     gon.labels = []
     gon.bgcolor = []
+    color = ["##FF0000", "#FE9A2E", "#F7FE2E", "#0040FF", "#00FF40", "#DF01D7", "#00FFFF", "#8000FF", "#BFFF00", "#045FB4"]
+
     @schedule.schedule_plans.each do |schedule_plan|
-      gon.labels << schedule_plan.plan
       if schedule_plan.start_time > schedule_plan.end_time
         gon.data << 24 * 60 * 60 - (schedule_plan.start_time.to_i - schedule_plan.end_time.to_i)
       else
         gon.data << (schedule_plan.end_time.to_i - schedule_plan.start_time.to_i)
       end
+      gon.labels << schedule_plan.plan
       gon.bgcolor << color.sample
     end
 
     render 'new' if @schedule.invalid?
   end
+  
+  def create
+    @schedule = Schedule.new(schedule_params)
+    @schedule.user_id = current_user.id
+    if @schedule.save
+      redirect_to schedule_path(id: @schedule.id)
+    else
+      render 'new'
+    end
+  end
+
+
+
 
   def edit
   end
@@ -54,6 +60,23 @@ class SchedulesController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: @schedule.user_id)
+
+    gon.data = []
+    gon.labels = []
+    gon.bgcolor = []
+    color = ["##FF0000", "#FE9A2E", "#F7FE2E", "#0040FF", "#00FF40", "#DF01D7", "#00FFFF", "#8000FF", "#BFFF00", "#045FB4"]
+
+    @schedule.schedule_plans.each do |schedule_plan|
+      if schedule_plan.start_time > schedule_plan.end_time
+        gon.data << 24 * 60 * 60 - (schedule_plan.start_time.to_i - schedule_plan.end_time.to_i)
+      else
+        gon.data << (schedule_plan.end_time.to_i - schedule_plan.start_time.to_i)
+      end
+      gon.labels << schedule_plan.plan
+      gon.bgcolor << color.sample
+    end
+
   end
 
   private
