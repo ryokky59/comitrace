f85c90aed1e9dd3ca2c256d590eddb7d68b8d5b9
ryo_ishibashi <ryokky59@gmail.com>
2018-09-20 10:01:25 +0900

    お気に入りボタンを非同期処理に変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,28 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+
   def create
-    favorite = current_user.favorites.create(schedule_id: params[:schedule_id])
-    redirect_to schedules_url
+    @favorite = current_user.favorites.create(schedule_id: params[:schedule_id])
+    @schedule = Schedule.find(params[:schedule_id])
+    respond_to do |format|
+      if @favorite.save
+        format.js
+      else
+        format.html { redirect_to schedule_path(@schedule) }
+      end
+    end
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to schedules_url
+    favorite = current_user.favorites.find_by(id: params[:id])
+    @schedule = favorite.schedule
+    respond_to do |format|
+      if favorite.destroy
+        format.js
+      else
+        format.html { redirect_to schedule_path(@schedule) }
+      end
+    end
   end
 end
