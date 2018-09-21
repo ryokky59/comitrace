02881125287d2a1c3e71325efee26c61ce342389
ryo_ishibashi <ryokky59@gmail.com>
2018-09-21 13:11:39 +0900

    スケジュール詳細画面を更新

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $('#schedule_plans')
     .on('cocoon:before-insert', function() {
-    if($('#schedule_plans .nested-fields').length >= 14) {
-          $('#add-link').hide();
-    } else {
-          $('#add-link').show();
-    }
-  })
+      if($('#schedule_plans .nested-fields').length >= 14) {
+        $('#add-link').hide();
+      } else {
+        $('#add-link').show();
+      }
+    })
+    .on('cocoon:after-remove', function() {
+      if($('#schedule_plans .nested-fields').length = 14) {
+        $('#add-link').show();
+      }
+    })
 });
